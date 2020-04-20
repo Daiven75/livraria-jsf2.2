cea4f629fee4c937831824316e1a4aac539de92f
Lucas Silva <lucas.s.l75@hotmail.com>
2020-04-19 22:14:43 -0300

    aprimorando grafico

--- src/br/com/caelum/livraria/bean/VendasBean.java
+++ src/br/com/caelum/livraria/bean/VendasBean.java
@@ -6,7 +6,8 @@ import java.util.Random;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 
@@ -20,27 +21,45 @@ public class VendasBean {
 	public BarChartModel getVendasModel() {
 		
 		 BarChartModel model = new BarChartModel();
-		 
-	     ChartSeries vendaSerie = new ChartSeries();
-	     vendaSerie.setLabel("Vendas 2020");
+		 model.setAnimate(true);
+	     ChartSeries vendaSerie2020 = new ChartSeries();
+	     vendaSerie2020.setLabel("Vendas 2020");
 	     
-	     List<Venda> vendas = getVendas(); 
+	     List<Venda> vendas = getVendas(1234); 
 	     
 	     for(Venda venda : vendas) {
-	       	vendaSerie.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+	       	vendaSerie2020.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+	     }
+	     
+	     ChartSeries vendaSerie2019 = new ChartSeries();
+	     vendaSerie2019.setLabel("Vendas 2019");
+	     
+	     vendas = getVendas(4321);
+	     for(Venda venda : vendas) {
+	    	vendaSerie2019.set(venda.getLivro().getTitulo(), venda.getQuantidade());
 	     }
 	 
-	     model.addSeries(vendaSerie);
-	 
+	     model.addSeries(vendaSerie2019);
+	     model.addSeries(vendaSerie2020);
+	     
+	     model.setTitle("Vendas");
+	     model.setLegendPosition("ne");
+	     
+	     Axis xAxis = model.getAxis(AxisType.X);
+	     xAxis.setLabel("Título");
+	     
+	     Axis yAxis = model.getAxis(AxisType.Y);
+	     yAxis.setLabel("Quantidade");
+	     
 	     return model;
 	}
 	
-	public List<Venda> getVendas() {
+	public List<Venda> getVendas(long seed) {
 
 		List<Livro> livros = new DAO<Livro>(Livro.class).listaTodos();
 		List<Venda> vendas = new ArrayList<Venda>();
 		
-		Random random = new Random(1234);
+		Random random = new Random(seed);
 		for(Livro livro : livros) {
 			Integer quantidade = random.nextInt(500);
 			vendas.add(new Venda(livro, quantidade));
